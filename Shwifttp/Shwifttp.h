af11d54eca731f21dfb7f5cbb851bd4fb7b5a004
hungrxyz <marko@hungry.dev>
2024-10-03 00:29:09 +0200

    Initial Commit

---
+++ Shwifttp/Shwifttp.h
@@ -0,0 +1,18 @@
+//
+//  Shwifttp.h
+//  Shwifttp
+//
+//  Created by marko on 03/10/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Shwifttp.
+FOUNDATION_EXPORT double ShwifttpVersionNumber;
+
+//! Project version string for Shwifttp.
+FOUNDATION_EXPORT const unsigned char ShwifttpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shwifttp/PublicHeader.h>
+
+
---
+++ ShwifttpTests/ShwifttpTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShwifttpTests.swift
+//  ShwifttpTests
+//
+//  Created by marko on 03/10/2024.
+//
+
+import Testing
+@testable import Shwifttp
+
+struct ShwifttpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
